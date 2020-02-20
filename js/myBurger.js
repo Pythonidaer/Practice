55719e9c71e6deced3bb54814281c7492ee07e94
Jonathan M. Hammond <Pythonidaer@gmail.com>
2020-02-20 18:02:24 -0500

    Successful Hamburger Menu Customization
    So, I finally analyzed Traversy's hamburger menu set up enough to recreate and customize my own.
    
    There's plenty that I'd still like to do to this one, but for now this is good enough. I spent a couple hours on it.
    
    Also,  the things I have to do next are customize the main nav-bar with grid instead of flex. I want to see how I can incorporate flex within grid, so that I can have the benefits of both where I need it.
    
    Next, I should enclose the main nav witin a header, just to make it more semantically accurate.
    
    After that, I should hide all flex items besides my logo "Jonnovative" on mobile devices.
    
    Once I have the list items appear differently on mobile tablet and desktop, I should recreate the design and leave this as a template.
    
    That way, I can first practice integrating flexbox within grid. After that, I'll work on integrating my initial (desktop-first) grid header nav-bar design into the hamburger design I just made.
    
    Once that's complete, I'll have successfully customized a mobile hamburger side-nav bar, as well as a desktop nav-bar, positioned how I want them and with flaticons as well.
    
    After all this is complete, I should integrate it into the entire website and work on CSS Critters again. Once I get enough practice with making a gallery/cards and whatnot, I'll make a dummy 4x4 or something template and work on filling each card with completed websites I've either done or will do.
    
    Then I'll straighten everything out with media queries.
    
    After that, I'll work on making my Contact Form functional FIRST.
    
    Then, I'll customize the form so that it's mobile-first and desktop/ipad sexy.
    
    Once the contact menu actually sends emails to my ecowebhosting address, I'll work on re-customizing my blog so that it allows you to search for a blog based off some type of keyword or category.
    
    Ultimately, I'd like to have certain "blog cards" appear based off the keyword input, but that might involve programming languages. Right now, I just want to ease into using a search bar.

---
+++ js/myBurger.js
@@ -0,0 +1,9 @@
+function openSlideMenu(){
+  document.getElementById('side-menu').style.width = '100vw';
+  // document.getElementById('main').style.marginRight = '250px';
+}
+
+function closeSlideMenu(){
+  document.getElementById('side-menu').style.width = '0';
+  // document.getElementById('main').style.marginRight = '0';
+}
