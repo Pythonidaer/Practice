b5dd36e592dc2d307fe33b71ff0446064ff1ab28
Jonathan M. Hammond <Pythonidaer@gmail.com>
2020-06-10 18:29:01 -0400

    Working on adding the fetch API
    Seeing if this course works.

--- portfolio/pages/tinker25stories.js
+++ portfolio/pages/tinker25stories.js
@@ -1,6 +1,40 @@
 // go out of pages directory
 import view from '../utils/tinker25view.js';
 
-export default function Stories(path) {
+export default async function Stories(path) {
+  const stories = await getStories(path);
+  console.log(stories);
+
   view.innerHTML = `<div>${path}</div>`;
 }
+
+async function getStories(path) {
+  const isHomeRoute =  path === '/';
+  if (isHomeRoute) {
+    path = '/news';
+  }
+  const response = await fetch(`https://node-hnapi.herokuapp.com${path}`);
+  const stories = await response.json();
+  return stories;
+}
+
+// https://node-hnapi.herokuapp.com
+
+// https://node-hnapi.herokuapp.com/new
+
+
+// https://node-hnapi.herokuapp.com/ask
+
+
+// https://node-hnapi.herokuapp.com/show
+
+// https://node-hnapi.herokuapp.com/jobs
+
+
+
+// / (Top) -> /new
+// /new (New) -> /newest
+// /ask (Ask) -> /ask
+// /show (Show) -> /show
+
+// /jobs (Jobs) -> /jobs
