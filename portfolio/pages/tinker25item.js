df294c11b939fce0cafb066a01164bc203ec82a0
Jonathan M. Hammond <Pythonidaer@gmail.com>
2020-06-13 16:12:22 -0400

    Console logging
    window.location.hash to see what the object logs in the console.

--- portfolio/pages/tinker25item.js
+++ portfolio/pages/tinker25item.js
@@ -1,5 +1,10 @@
 import view from '../utils/tinker25view.js';
 
 export default function Item() {
+  getStory();
   view.innerHTML = `<div>item</div>`
+}
+
+function getStory() {
+  console.log(window.location.hash);
 }
