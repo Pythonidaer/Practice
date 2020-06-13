3ba04f3557cc06a10447fe74120ab2264ae58612
Jonathan M. Hammond <Pythonidaer@gmail.com>
2020-06-13 18:13:55 -0400

    Testing Add to Favorites result

--- portfolio/pages/tinker25favorites.js
+++ portfolio/pages/tinker25favorites.js
@@ -1,5 +1,16 @@
-import view from '../utils/tinker25view.js'
+import view from '../utils/tinker25view.js';
+import checkFavorite from '../utils/checkFavorite.js';
+import store from '../tinker25store.js';
+import Story from '../components/Story.js'
 
 export default function Favorites() {
-  view.innerHTML = `<div>Favorites</div>`
+  const { favorites } = store.getState();
+  const hasFavorites = favorites.length > 0;
+
+  view.innerHTML = `<div>
+    ${hasFavorites ? favorites.map(story => Story({
+      ...story,
+      isFavorite: checkFavorite(favorites, story)
+    })).join('') : "Add some favorites!"}
+  </div>`
 }
