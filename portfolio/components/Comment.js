b48d6031097c5e117d489165fb45895725d4fed5
Jonathan M. Hammond <Pythonidaer@gmail.com>
2020-06-13 17:03:44 -0400

    Utilizing template literals with css
    This also includes utilizing HTML within a return of an exported function. There is much more to this than what the title states. This upload will provide dynamic styling on nested comment indentation based on the level of the comment.

--- portfolio/components/Comment.js
+++ portfolio/components/Comment.js
@@ -3,7 +3,7 @@ export default function Comment(comment) {
   const hasNestedComments = comment.comments.length > 0;
 
   return `
-    <div class="nested-comments-0">
+    <div class="nested-comments-${comment.level}">
       <p class="comment-header">
         ${comment.user} | ${comment.time_ago}
       </p>
