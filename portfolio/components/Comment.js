34e8c8a193afeccbfadc97a2dd5501f09029c7c7
Jonathan M. Hammond <Pythonidaer@gmail.com>
2020-06-13 17:01:15 -0400

    Resolving Error
    Typo led to unidenfited

--- portfolio/components/Comment.js
+++ portfolio/components/Comment.js
@@ -8,7 +8,7 @@ export default function Comment(comment) {
         ${comment.user} | ${comment.time_ago}
       </p>
       ${comment.content}
-      ${hasNestedComments ? comment.comments map(comment => Comment(comment)).join("") : ""}
+      ${hasNestedComments ? comment.comments.map(comment => Comment(comment)).join("") : ""}
     </div>
   `
 }
