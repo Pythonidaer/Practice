af71b52014d8bcacc69f93159c88650cca086e77
Jonathan M. Hammond <Pythonidaer@gmail.com>
2020-06-13 17:30:22 -0400

    enclosing objects within cases

--- portfolio/tinker25store.js
+++ portfolio/tinker25store.js
@@ -15,14 +15,16 @@ const initialState = {
 
 function favoritesReducer(state = initialState, action) {
   switch (action.type) {
-    case "ADD_FAVORITE":
+    case "ADD_FAVORITE": {
       const addedFavorite = action.payload.favorite;
       const favorites = [...state.favorites, addedFavorite];
       return { favorites };
-    case "REMOVE_FAVORITE":
+    }
+    case "REMOVE_FAVORITE": {
       const removedFavorite = action.payload.favorite;
       const favorites = state.favorites.filter(favorite => favorite.id !== removedFavorite.id);
       return { favorites };
+    }
     default:
       return state;
   }
