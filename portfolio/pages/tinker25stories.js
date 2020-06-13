31c0236fbcec6dc2d0000474bc8d6ad8727ea03c
Jonathan M. Hammond <Pythonidaer@gmail.com>
2020-06-13 17:57:25 -0400

    import issue
    Corrected import typo name

--- portfolio/pages/tinker25stories.js
+++ portfolio/pages/tinker25stories.js
@@ -2,7 +2,7 @@ import Story from '../components/Story.js';
 import view from '../utils/tinker25view.js';
 import baseUrl from '../utils/baseUrl.js';
 import checkFavorite from '../utils/checkFavorite.js';
-import store from '../store.js';
+import store from '../tinker25store.js';
 
 export default async function Stories(path) {
   const { favorites } = store.getState();
