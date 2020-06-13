25c39dc9b781e2307f83a886907c22af55813bd3
Jonathan M. Hammond <Pythonidaer@gmail.com>
2020-06-13 16:22:51 -0400

    No Comments

--- portfolio/pages/tinker25item.js
+++ portfolio/pages/tinker25item.js
@@ -1,10 +1,22 @@
+import Story from '../components.Story.js';
 import view from '../utils/tinker25view.js';
 
-export default function Item() {
-  getStory();
-  view.innerHTML = `<div>item</div>`
+export default async function Item() {
+  const story = await getStory();
+  const hasComments = story.comments.length > 0;
+
+  view.innerHTML = `
+    <div>
+      ${Story(story)}
+    </div>
+    <hr/>
+    ${hasComments ? story.comments.map(comment => JSON.stringify(comment)).join('') : 'No comments')}
+    `
 }
 
 function getStory() {
-  console.log(window.location.hash);
+  const storyId = window.location.hash.split('?id=')[1];
+  const response = await fetch(`https://node-hnapi.herokuapp.com/item/${storyId}`);
+  const story = await response.json();
+  return story;
 }
